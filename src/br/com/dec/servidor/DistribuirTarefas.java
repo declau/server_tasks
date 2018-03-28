ad4135d488aa16e3314c8ae3ab97017f3efda8c7
declau <dec82@hotmail.com>
2018-03-27 21:02:51 -0300

    Second Step

--- src/br/com/dec/servidor/DistribuirTarefas.java
+++ src/br/com/dec/servidor/DistribuirTarefas.java
@@ -1,5 +1,6 @@
 package br.com.dec.servidor;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -19,16 +20,36 @@ public class DistribuirTarefas implements Runnable {
 
 			Scanner entradaCliente = new Scanner(socket.getInputStream());
 
-			while(entradaCliente.hasNextLine()) {
+			PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+
+			while (entradaCliente.hasNextLine()) {
 				String comando = entradaCliente.nextLine();
-				System.out.println(comando);
+				System.out.println("Comando recebido " + comando);
+
+				switch (comando) {
+				
+					case "C1": {
+						saidaCliente.println("Confirma comando C1");
+						break;
+					}
+	
+					case "C2": {
+						saidaCliente.println("Confirma comando C2");
+						break;
+					}
+					default: {
+						saidaCliente.println("Comando nao encontrado");
+					}
+					}
+	
+					System.out.println(comando);
 			}
-			
+
+			saidaCliente.close();
 			entradaCliente.close();
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 
 	}
-
 }
